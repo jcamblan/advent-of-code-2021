145d5a1f352a6c9745112ea5a678a494c6bdf075
Julien <julien.camblan@gmail.com>
2021-12-01 08:27:21 +0100

    feat: add day 1 part 2

--- 01/day01.rb
+++ 01/day01.rb
@@ -5,6 +5,21 @@ class Day01
   end
 
   def solution
-    @values.each_cons(2).count { |previous_value, value| previous_value < value }
+    { "1" => part1, "2" => part2 }
+  end
+
+  private
+
+  def part1
+    count_increasing_values(@values)
+  end
+
+  def part2
+    grouped_inputs = @values.each_cons(3).map(&:sum)
+    count_increasing_values(grouped_inputs)
+  end
+
+  def count_increasing_values(array)
+    array.each_cons(2).count { |previous_value, value| previous_value < value }
   end
 end
