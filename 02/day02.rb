256e024159e71e12a8dc9985717cf8b71b554f4b
Julien <julien.camblan@gmail.com>
2021-12-02 08:07:57 +0100

    feat: resolves day2 part 1

--- 02/day02.rb
+++ 02/day02.rb
@@ -1,7 +1,14 @@
 class Day02
   def initialize
-    input = File.open('./02/input.txt').read
+    @instructions = File.readlines('./02/input.txt')
+                        .map { |l| l.split(' ') }
+                        .group_by(&:shift)
+                        .map { |k,v| [k, v.flatten.map(&:to_i).sum] }.to_h
+    @horizontal_position = @instructions['forward']
+    @depth = @instructions['down'] - @instructions['up']
   end
 
-  def solution; end
+  def solution
+   { '1' => @horizontal_position * @depth }
+  end
 end
