a5da6a3c80d411e69fd127b336fe76ae1a14d515
Julien <julien.camblan@gmail.com>
2021-12-02 08:31:35 +0100

    feat: resolves day2 part 2

--- 02/day02.rb
+++ 02/day02.rb
@@ -1,14 +1,37 @@
 class Day02
   def initialize
-    @instructions = File.readlines('./02/input.txt')
-                        .map { |l| l.split(' ') }
-                        .group_by(&:shift)
-                        .map { |k,v| [k, v.flatten.map(&:to_i).sum] }.to_h
-    @horizontal_position = @instructions['forward']
-    @depth = @instructions['down'] - @instructions['up']
+    @movements = File.readlines('./02/input.txt').map { |l| l.split(' ') }
+    @horizontal_position = 0
+    @depth = 0
+    @aim = 0
   end
 
   def solution
-   { '1' => @horizontal_position * @depth }
+    { '2' => result_part_2 }
+  end
+
+  def result_part_1
+    instructions = @movements.dup
+    instructions.group_by(&:shift)
+                .map { |k,v| [k, v.flatten.map(&:to_i).sum] }.to_h
+
+    instructions['forward'] * (instructions['down'] - instructions['up'])
+  end
+
+  def result_part_2
+    @movements.each do |direction, length|
+      length = length.to_i
+      case direction
+      when 'forward'
+        @horizontal_position += length
+        @depth += @aim * length
+      when 'down'
+        @aim += length
+      when 'up'
+        @aim -= length
+      end
+    end
+
+    @horizontal_position * @depth
   end
 end
