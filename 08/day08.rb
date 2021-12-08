6d59962b5c2fc964f8f24a8b53fbaf48ce8b7673
Julien <julien.camblan@gmail.com>
2021-12-09 00:05:51 +0100

    refactor: satisfy rubocop

--- 08/day08.rb
+++ 08/day08.rb
@@ -47,36 +47,60 @@ class Day08
   def part2
     @lines = @input.lines.map { _1.split(' | ').map(&:split) }
     @lines.map do |signal_pattern, output|
-      fill_values(signal_pattern)
-
-      translate_output(output)
+      @signal_pattern = signal_pattern
+      translate(output)
     end.sum
   end
 
-  def fill_values(signal_pattern)
-    ez_numbers = signal_pattern.filter_map do |digits|
-      [EZ_SEGMENTS[digits.length], digits.chars] if EZ_SEGMENTS[digits.length]
-    end.to_h
+  private
 
-    @values = {
-      'a' => (ez_numbers[7] - ez_numbers[1]).first,
-      'c' => ez_numbers[1].find { signal_pattern.join.count(_1) == 8 },
-      'f' => ez_numbers[1].find { signal_pattern.join.count(_1) == 9 },
-      'b' => (ez_numbers[4] - ez_numbers[1]).find { signal_pattern.join.count(_1) == 6 },
-      'd' => (ez_numbers[4] - ez_numbers[1]).find { signal_pattern.join.count(_1) == 7 }
-    }.tap do |values|
-      abcdf = values.values.flatten
-      nine = signal_pattern.find do |n|
-        abcdf.all? { n.chars.include?(_1) } && n.length == 6
-      end
-
-      values['g'] = (nine.chars - abcdf).first
-      values['e'] = (%w[a b c d e f g] - values.values.flatten).last
-    end
+  def translate(output)
+    # been block without any understanding for hours on part 2 because here I wrote @values[char] 😭
+    output.map { |digits| NUMBERS[digits.chars.map { |char| values.invert[char] }.sort].to_s }.join.to_i
   end
 
-  def translate_output(output)
-    # been block without any understanding for hours on part 2 because here I wrote @values[char] 😭
-    output.map { |digits| NUMBERS[digits.chars.map { |char| @values.invert[char] }.sort].to_s }.join.to_i
+  def values
+    abcdf_values.merge(ge_values)
+  end
+
+  def abcdf_values
+    {
+      'a' => (ez_numbers[7] - ez_numbers[1]).first,
+      'c' => find_by_recurrency(ez_numbers[1], 8),
+      'f' => find_by_recurrency(ez_numbers[1], 9),
+      'b' => find_by_recurrency(bd_chars, 6),
+      'd' => find_by_recurrency(bd_chars, 7)
+    }
+  end
+
+  def ge_values
+    {
+      'g' => g_char,
+      'e' => (%w[a b c d e f g] - abcdf_values.values.flatten - [g_char]).last
+    }
+  end
+
+  # return the entry from selection that appears the given amount of times in the entire signal pattern
+  def find_by_recurrency(array, count)
+    array.find { @signal_pattern.join.count(_1) == count }
+  end
+
+  # 1, 4, 7 and 8 are easy to isolate because of their unique amount of segments
+  def ez_numbers
+    @signal_pattern.filter_map do |digits|
+      [EZ_SEGMENTS[digits.length], digits.chars] if EZ_SEGMENTS[digits.length]
+    end.to_h
+  end
+
+  # b and d segments can be deducted easily from 1 and 4 values
+  def bd_chars
+    (ez_numbers[4] - ez_numbers[1])
+  end
+
+  # once we got values from a, b, c, d & f, we can easily deduce the value of g
+  # thanks to digit nine that is the only one to include all of those segments
+  def g_char
+    nine = @signal_pattern.find { (abcdf_values.values.flatten - _1.chars).empty? && _1.length == 6 }
+    (nine.chars - abcdf_values.values.flatten).first
   end
 end
