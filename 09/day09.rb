b9f8969d0e0724f832a9d8ed6a81373f404b15f9
Julien <julien.camblan@gmail.com>
2021-12-09 07:53:35 +0100

    feat: resolves day 9 part 1

---
+++ 09/day09.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# Day 8: Seven Segment Search
+class Day09
+  def initialize(input = nil)
+    @input = input || File.open('./09/input.txt').read
+    @matrix = @input.lines(chomp: true).map(&:chars)
+  end
+
+  def part1
+    @matrix.filter_map.with_index do |row, vind|
+      row.filter_map.with_index do |point, hind|
+        borders = [[1, 0], [0, -1], [0, 1], [-1, 0]].filter_map do |y, x|
+          ny = [vind + y, 0].max
+          nx = [hind + x, 0].max
+          @matrix.dig(ny, nx) unless ny == vind && nx == hind
+        end
+        point.to_i + 1 if borders.all? { _1 > point }
+      end
+    end.flatten.sum
+  end
+
+  def part2; end
+end
---
+++ 09/test_day09.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+# Ensure that solution for the day works with examples
+class TestDay09 < Minitest::Test
+  def input
+    <<~INPUT
+      2199943210
+      3987894921
+      9856789892
+      8767896789
+      9899965678
+    INPUT
+  end
+
+  def setup
+    @resolver = Day09.new(input)
+  end
+
+  def test_part1_result
+    assert_equal 15, @resolver.part1
+  end
+
+  def test_part2_result
+    assert_equal 61_229, @resolver.part2
+  end
+end
