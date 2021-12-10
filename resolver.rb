ca1f2244140f4ba15ab0e9507cdf443cc205beef
Julien <julien.camblan@gmail.com>
2021-12-10 23:37:46 +0100

    feat: run only last day

--- resolver.rb
+++ resolver.rb
@@ -9,13 +9,35 @@ require 'pry'
 class AdventResolver
   COMPLETED_DAYS = Dir['./*/'].sort.map { |f| Object.const_get("Day#{f.delete('/').delete('.')}") }
 
-  def self.results
-    rows = COMPLETED_DAYS.map.with_index(1) do |klass, index|
-      [index, klass.new.part1, klass.new.part2]
-    end
+  def self.all
+    table(
+      COMPLETED_DAYS.map.with_index(1) do |klass, index|
+        [index, klass.new.part1, klass.new.part2]
+      end
+    )
+  end
 
+  def self.last
+    klass = COMPLETED_DAYS.last
+    table([[COMPLETED_DAYS.size, klass.new.part1, klass.new.part2]])
+  end
+
+  def self.day(index)
+    klass = COMPLETED_DAYS[index]
+    table([[index, klass.new.part1, klass.new.part2]])
+  end
+
+  def self.table(rows)
     Terminal::Table.new headings: ['Day', 'Part 1', 'Part 2'], rows: rows
   end
 end
 
-puts AdventResolver.results
+day = ARGV.find { _1.to_i.to_s == _1 && _1 != 0 }
+
+if ARGV.include?('all')
+  puts AdventResolver.all
+elsif day
+  puts AdventResolver.day(day.to_i)
+else
+  puts AdventResolver.last
+end
