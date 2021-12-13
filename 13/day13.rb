823a9a470e7501e46098f77bb78436caf2fc4b19
Julien <julien.camblan@gmail.com>
2021-12-13 10:32:06 +0100

    feat: resolves day 13 part 2

--- 13/day13.rb
+++ 13/day13.rb
@@ -7,26 +7,38 @@ class Day13
     raw_dots, raw_folds = raw.split("\n\n").map { _1.lines(chomp: true) }
     @dots = raw_dots.map { _1.split(',').map(&:to_i) }
     @folds = raw_folds.map { _1.split(' ')[-1].split('=') }
+    @paper = blank_paper
+    @dots.each { fill_paper!(_1) }
   end
 
   def part1
-    paper = blank_paper
-    @dots.each { fill_paper!(paper, _1) }
-
-    paper = fold(paper, @folds.first)
-    paper.flatten.count(&:positive?)
+    fold(@folds.first)
+    @paper.flatten.count(&:positive?)
   end
 
-  def part2; end
+  def part2
+    @folds.each { fold(_1) }
+    display_paper
+    @paper.flatten.count(&:positive?)
+  end
 
   private
 
-  def fold(paper, instruction)
+  def display_paper
+    puts ''
+    puts 'REAL DAY 13 PART 2 SOLUTION :'
+    @paper.each do |a|
+      puts a.map { |dot| dot.positive? ? '#' : ' ' }.join
+    end
+    puts ''
+  end
+
+  def fold(instruction)
     case instruction[0]
     when 'y'
-      vertical_fold(paper, instruction[1].to_i)
+      @paper = vertical_fold(@paper, instruction[1].to_i)
     when 'x'
-      horizontal_fold(paper, instruction[1].to_i)
+      @paper = horizontal_fold(@paper, instruction[1].to_i)
     end
   end
 
@@ -36,12 +48,12 @@ class Day13
     part1.zip(part2.reverse).map { |a, b| a.zip(b).map(&:sum) }
   end
 
-  def horizontal_fold(paper, index)
-    vertical_fold(paper.transpose, index).transpose
+  def horizontal_fold(_paper, index)
+    vertical_fold(@paper.transpose, index).transpose
   end
 
-  def fill_paper!(paper, dot)
-    paper[dot[1]][dot[0]] += 1
+  def fill_paper!(dot)
+    @paper[dot[1]][dot[0]] += 1
   end
 
   def blank_paper
